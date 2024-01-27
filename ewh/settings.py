9a26e8f784b7effb73b82e0006e2aa0401b6e438
HitzCommitted <hitzcommitted@gmail.com>
2024-01-27 10:52:12 +0300

    added postgres config

--- ewh/settings.py
+++ ewh/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'ewh.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ewh_db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 

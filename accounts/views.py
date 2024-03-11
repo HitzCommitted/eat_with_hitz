4c3369b4ae3c20b2baf4cabc882153cf5f664f05
HitzCommitted <hitzcommitted@gmail.com>
2024-03-11 20:56:51 +0300

    Creating user using create_user method. Hashing pwd as well.

--- accounts/views.py
+++ accounts/views.py
@@ -9,9 +9,22 @@ def registerUser(request):
     if request.method == "POST":
         form = UserForm(request.POST)
         if form.is_valid():
-            user = form.save(commit=False)
+            # Create user using create user method
+            first_name = form.cleaned_data["first_name"]
+            last_name = form.cleaned_data["last_name"]
+            username = form.cleaned_data["username"]
+            email = form.cleaned_data["email"]
+            password = form.cleaned_data["password"]
+            user = User.objects.create_user(
+                first_name=first_name,
+                last_name=last_name,
+                username=username,
+                email=email,
+                password=password,
+            )
             user.role = User.CUSTOMER
             user.save()
+
             return redirect("registerUser")
     else:
         form = UserForm()

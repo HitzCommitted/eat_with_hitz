40f0b799edc0bf925127dec002b981274de96943
HitzCommitted <hitzcommitted@gmail.com>
2024-03-16 22:41:06 +0300

    Activate users by email

--- accounts/views.py
+++ accounts/views.py
@@ -7,6 +7,8 @@ from django.contrib import messages, auth
 from .utils import detectUser, send_verification_email
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.core.exceptions import PermissionDenied
+from django.utils.http import urlsafe_base64_decode
+from django.contrib.auth.tokens import default_token_generator
 
 
 # Create your views here.
@@ -165,4 +167,17 @@ def vendorDashboard(request):
 
 def activate(request, uidb64, token):
     # Activate the user by setting is_active to True
-    return
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = User._default_manager.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request, "Congratulations! Your account is activated")
+        return redirect("myAccount")
+    else:
+        message.error(request, "Activation link is invalid")
+        return redirect("myAccount")
